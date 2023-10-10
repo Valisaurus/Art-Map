47394dd2723822147ea9f5af9402eb86625b05e8
Valisaurus <valley.alosachi@hotmail.com>
2023-10-10 11:40:45 +0200

    added an admin page

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+
+const config = defineConfig({
+  projectId: "z4x2zjsw",
+  dataset: "production",
+  title: "Art Map",
+  apiVersion: "2023-10-10",
+  basePath: "/admin",
+  plugins: [deskTool()],
+});
+
+export default config;
