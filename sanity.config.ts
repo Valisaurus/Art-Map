c1c72a071437f98fb2906ebaf3cbdc8e571bad92
AnnaBrum <annahelenabrumark@gmail.com>
2023-10-16 12:46:49 +0200

    created form document type

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "z4x2zjsw",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-10-10",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: schemas }
 });
 
 export default config;
--- types/form.ts
+++ types/form.ts
@@ -4,8 +4,6 @@ export type Form = {
   _id: string;
   _createdAt: Date;
   name: string;
-  slug: string;
-  image: string;
-  url: string;
-  content: PortableTextBlock[];
+  address: object;
+  about: Text;
 };
