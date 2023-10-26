d029b7807bee3a58999f3758f391dbc88f8bce07
AnnaBrum <annahelenabrumark@gmail.com>
2023-10-26 09:51:54 +0200

    fixed: styled map to fill out startpage

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -128,7 +128,7 @@ const MapComponent = () => {
     }
   }, [map, locations]);
 
-  return <div id="map" style={{ width: "100%", height: "400px" }} />;
+  return <div id="map" style={{ width: "100%", height: "auto" }} />;
 };
 
 export default MapComponent;
