9dafa48364660eb5f7afb40d641da828c4f51072
AnnaBrum <annahelenabrumark@gmail.com>
2023-10-26 15:30:22 +0200

    update: started styling on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,23 @@
 import Link from "next/link";
 import Messages from "./messages";
+import styles from "./login.module.css";
 
 export default function Login() {
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-in"
-        method="post"
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
+    <div className={styles.formWrapper}>
+      <form className={styles.form} action="/auth/sign-in" method="post">
+        <label htmlFor="email">Email</label>
+        <input name="email" placeholder="you@example.com" required />
+        <label htmlFor="password">Lösenord</label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
           type="password"
           name="password"
           placeholder="••••••••"
           required
         />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In
-        </button>
-        <button
-          formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
-        >
-          Sign Up
+        <button className={styles.loginButton}>Logga in</button>
+        <button className={styles.registerButton} formAction="/auth/sign-up">
+          Registrera konto
         </button>
         <Messages />
       </form>
