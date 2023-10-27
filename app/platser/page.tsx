53a7f15b3fdb551577310cbcb459da58745158f9
Valisaurus <valley.alosachi@hotmail.com>
2023-10-27 10:22:29 +0200

    add: fetched gallerinames and published on platser

--- app/platser/page.tsx
+++ app/platser/page.tsx
@@ -1,9 +1,41 @@
+"use client";
 import { createClient } from "@sanity/client";
+import { useEffect, useState } from "react";
+
+const client = createClient({
+  projectId: "z4x2zjsw",
+  dataset: "production",
+  useCdn: false,
+});
+
+type venueName = {
+  name: string;
+};
 
 export default function Venues() {
+  const [venueNames, setVenueNames] = useState<venueName[]>([]);
+
+  useEffect(() => {
+    const documentType = "form"; //
+
+    client
+      .fetch<venueName[]>(`*[_type == "${documentType}"]`)
+      .then((fetchedName) => {
+        // Handle the fetched data
+        setVenueNames(fetchedName);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div>
-      <p>This is the venues site Hej hej</p>
+      <ul>
+        {venueNames.map((venueName) => (
+          <li key={venueName.name}>{venueName.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
