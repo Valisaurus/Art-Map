0602fb424c33722e6c3ab50f77388abbf900dadd
AnnaBrum <annahelenabrumark@gmail.com>
2023-11-01 11:34:16 +0100

    Added: code to slask-page

---
+++ app/slask/page.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Slask() {
+  return (
+    <div >
+      <form  action="/auth/status" method="post">
+        {/* <label htmlFor="email">Email</label>
+        <input name="email" placeholder="you@example.com" required />
+        <label htmlFor="password">Lösenord</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          required
+        /> */}
+        <button >Registera konto</button>
+
+      </form>
+    </div>
+  );
+}
