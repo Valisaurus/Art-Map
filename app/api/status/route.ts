362060831812a310ff8c1247e2f7a8eb3294ab5a
Valisaurus <valley.alosachi@hotmail.com>
2023-11-01 11:34:41 +0100

    changed: code in rounting

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -3,10 +3,13 @@ import { cookies } from "next/headers";
 import { createClient } from "@sanity/client";
 import { Application } from "../../../types/application";
 import { User } from "@supabase/supabase-js";
+import application from "@/sanity/schemas/documents/application";
+
 const client = createClient({
   projectId: "z4x2zjsw",
   dataset: "production",
   useCdn: false,
+  apiVersion: "2023-10-10",
 });
 
 export const dynamic = "force-dynamic";
@@ -17,25 +20,28 @@ export default async function setStatus(user: User) {
   const documentType = "application";
 
   try {
-    const fetchedapplication = await client.fetch<
+    const fetchedApplications = await client.fetch<
       Application[]
     >(`*[_type == "${documentType}"]{
-     
+     _id
       status,
     }`);
 
-    fetchedapplication.map(async (application) => {
-      if (application.status === "approved") {
-        const { error } = await supabase
-          .from("status")
-          .update({ status: "approved" })
-          .eq("user_id", user.id);
+    const applicationsToUpdate = fetchedApplications.filter(
+      (application) => application.status === "approved"
+    );
 
-        if (error) {
-          console.error("Error inserting data:", error);
-        }
+    const updateQueries = applicationsToUpdate.map(async (application) => {
+      const { error } = await supabase
+        .from("status")
+        .update({ status: "approved" })
+        .eq("user_id", user.id);
+      if (error) {
+        console.error("Error updating status:", error);
       }
+      console.log("Status updated for application ID", application._id);
     });
+    await Promise.all(updateQueries);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
