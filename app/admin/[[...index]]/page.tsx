e8b8eedd824d26ce06b0fa9dc760afe9658c53c6
Valisaurus <valley.alosachi@hotmail.com>
2023-11-02 13:55:11 +0100

    update: updated code in admin so form is sent with a route

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -4,38 +4,28 @@ import { config } from "@/sanity.config";
 import supabase from "@/supabase/utils/supabaseClient";
 import { useState } from "react";
 
-
-
 export default function AdminPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  const [ email, setEmail ] = useState<string>("");
-  const [ password, setPassword ] = useState<string>("");
-
-  async function handleSignUp() {
-    const { error } = await supabase.auth.signUp({ email, password });
-    if (error) {
-      console.log(error.message)
-    } else {
-      console.log("Magic link sent to " + email )
-    }
-  }
-
-  return ( 
+  return (
     <>
-  <NextStudio config={config} />
-  <div>
-    <input
-    type="email"
-    value={email}
-    onChange={(e) => setEmail(e.target.value)}
-    />
-    <input
-    type="password"
-    value={password}
-    onChange={(e) => setPassword(e.target.value)}
-    />
-    <button onClick={handleSignUp}>Sign up user</button>
-  </div>
-  </>
+      <NextStudio config={config} />
+      <div>
+        <form action="/auth/sign-up" method="POST">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Sign up user</button>
+        </form>
+      </div>
+    </>
   );
 }
