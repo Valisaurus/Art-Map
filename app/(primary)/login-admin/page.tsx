30d2109852c54761d7ab1ce7b3a9b2eb3c2b4a02
AnnaBrum <annahelenabrumark@gmail.com>
2023-11-06 12:01:36 +0100

    Update: removed session on admin login page

--- app/(primary)/login-admin/page.tsx
+++ app/(primary)/login-admin/page.tsx
@@ -1,21 +1,21 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from "next/headers";
-import { redirect } from 'next/navigation';
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from "next/headers";
+// import { redirect } from 'next/navigation';
 import Messages from "./messages";
 import styles from "./login.module.css";
 
 export const dynamic = "force-dynamic";
 
 export default async function LoginAdmin() {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  // const supabase = createServerComponentClient({ cookies });
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession();
 
-  if (session) {
-    // this is a protected route - only users who are signed in can view this route
-    redirect('/startpage');
-  }
+  // if (session) {
+  //   // this is a protected route - only users who are signed in can view this route
+  //   redirect('/startpage');
+  // }
 
   return (
     <div className={styles.formWrapper}>
