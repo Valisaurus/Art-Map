5c075aabc0bc42a8926cffe5362f16c29db55bcb
AnnaBrum <annahelenabrumark@gmail.com>
2023-11-14 22:29:24 +0100

    Created: exhibition card components, not sure if they will be used though...

---
+++ components/ExhibitionCard/ExhibitionCard.tsx
@@ -0,0 +1,16 @@
+import styles from "./ExhibitionCard.module.css";
+import { getExhibition } from "@/sanity/sanity.utils";
+
+type Props = {
+  params: { ustallning: string };
+};
+
+export default async function ExhibitionCard({ params }: Props) {
+  const slug = params.ustallning;
+  const exhibition = await getExhibition(slug);
+  return (
+    <div className={styles.cardWrapper}>
+
+    </div>
+  );
+}
