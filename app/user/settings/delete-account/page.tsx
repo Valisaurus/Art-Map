6b7a90ae652dd7d92f2bf96ff1715fa4ebc121a9
Valisaurus <valley.alosachi@hotmail.com>
2023-11-16 09:26:55 +0100

    add: logic for deleting a user

--- app/user/settings/delete-account/page.tsx
+++ app/user/settings/delete-account/page.tsx
@@ -10,10 +10,6 @@ const DeleteAccount = ({ user_id, user_email }: DeleteAccountProps) => {
       </div>
 
       <form action="/auth/delete-account" method="post">
-
-        <input type="hidden" name="user_id" value={user_id} />
-        <input type="hidden" name="user_email" value={user_email} />
-
         <label htmlFor="password">
           <i>Är du säker på att du vill ta bort ditt konto permanent?</i>
         </label>
