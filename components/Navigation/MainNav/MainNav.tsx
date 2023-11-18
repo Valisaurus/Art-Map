f7a9f10b97414d2e2b1c12df6ae4dd850834d574
AnnaBrum <annahelenabrumark@gmail.com>
2023-11-18 22:34:14 +0100

    Update: started making mobile menu

--- components/Navigation/MainNav/MainNav.tsx
+++ components/Navigation/MainNav/MainNav.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./MainNav.module.css";
+
 const Nav = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <nav className={styles.nav}>
+    <nav
+      className={`${styles.nav} ${isActive ? styles.active : ""}`} >
       <div className={styles.home}>
         <Link href="/">GBG ART GUIDE</Link>
       </div>
-      <div className={styles.list}>
+      <div className={`${styles.hamburger} ${isActive ? styles.active : ""}`} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+      <div className={`${styles.list} ${isActive ? styles.active : ""}`}>
         <Link href="/utstallningar">Utställningar</Link>
         <Link href="/event">Event</Link>
         <Link href="/platser">Platser</Link>
