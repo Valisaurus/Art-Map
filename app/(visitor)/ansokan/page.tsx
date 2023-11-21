5e1ecd9ed7b36d747e740dc67b50ce4f909fdeb7
AnnaBrum <annahelenabrumark@gmail.com>
2023-11-21 07:45:52 +0100

    Styling: about, login, ansokan

--- app/(visitor)/ansokan/page.tsx
+++ app/(visitor)/ansokan/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-// import styles from "./ansokan.module.css";
+import styles from "./ansokan.module.css";
 import ApplicationForm from "@/components/Forms/ApplicationForm/ApplicationForm";
 
 
 export default function Application() {
   return (
-    <>
+    <div className={styles.module}>
       <div >
         Hej!
-        <br />
+       
         Här fyller du i uppgifter om din verksamhet.
       </div>
       <ApplicationForm />
-    </>
+    </div>
   );
 }
